1f137bf51bf5f00332069f7f5319f453061b4eb5
Aran Murphy <aranwmurphy@gmail.com>
2020-10-18 18:38:17 -0400

    Prepped for next (incompatible version)

--- src/red-publisher.ts
+++ src/red-publisher.ts
@@ -1,6 +1,7 @@
 import { Redis } from "ioredis";
 
 export class RedPublisher {
+
     constructor(public readonly client: Redis) {}
 
     public async publish(channel: string, event: string): Promise<void> {
