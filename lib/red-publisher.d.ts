cc297ce816f392167009549fec24d18f9110ba16
Aran Murphy <aranwmurphy@gmail.com>
2020-10-31 15:28:32 -0400

    Built library and incremented version

--- lib/red-publisher.d.ts
+++ lib/red-publisher.d.ts
@@ -2,5 +2,5 @@ import { Redis } from "ioredis";
 export declare class RedPublisher {
     readonly client: Redis;
     constructor(client: Redis);
-    publish<TMessage>(channel: string, message: TMessage): Promise<void>;
+    publish(channel: string, message: any): Promise<void>;
 }
